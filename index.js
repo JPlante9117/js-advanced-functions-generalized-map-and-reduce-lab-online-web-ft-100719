08f9710cc13f3bd56f418b0ebd4b933cacafad2b
JPlante9117 <jacrplante@gmail.com>
2020-01-29 14:48:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(array, func){
+    const newArr = []
+    array.forEach(item =>{newArr.push(func(item))})
+    return newArr
+}
+
+function reduce(array, func, startingVal){
+    let returnVal = startingVal ? startingVal : array[0]
+    let i = startingVal ? 0 : 1
+    for(; i < array.length; i++){
+        returnVal = func(array[i], returnVal)
+    }
+    return returnVal
+}
